1a5d4a37d0ee1f5bd489215e02ed0f80c9a9b699
anirudhi89 <anirudh.iyer@outlook.com>
2023-11-16 20:31:29 -0500

    readme and file struct

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read the data from the CSV file
-data = pd.read_csv('data.csv')
+data = pd.read_csv('data/data.csv')
 
 # Define the MIGPLAC5 values for comparison
 countries = {'France': 421, 'Germany': 453, 'Mexico': 200, 'Japan': 501, 'China': 500}
@@ -12,8 +12,8 @@ usa_data = data[data['MIGPLAC5'].between(0, 120)]
 immigrants_data = data[data['MIGPLAC5'] > 120]
 
 # Save the split data to CSV files
-usa_data.to_csv('USA_wage.csv', index=False)
-immigrants_data.to_csv('immigrant_wage.csv', index=False)
+usa_data.to_csv('results/USA_wage.csv', index=False)
+immigrants_data.to_csv('results/immigrant_wage.csv', index=False)
 
 # Calculate average wages for USA (excluding 0 values)
 avg_USA_wage = usa_data[usa_data['US1940A_FAMERN'] != 99999]['US1940A_FAMERN'].mean()
@@ -26,9 +26,9 @@ for country, migplac5 in countries.items():
     avg_immigrant_wages[country] = avg_wage
 
 # Save the average wages to CSV files
-pd.Series(avg_USA_wage).to_csv('avg_USA_wage.csv', header=['Average_Wage'])
+pd.Series(avg_USA_wage).to_csv('results/avg_USA_wage.csv', header=['Average_Wage'])
 for country, avg_wage in avg_immigrant_wages.items():
-    pd.Series(avg_wage).to_csv(f'avg_{country}_wage.csv', header=['Average_Wage'])
+    pd.Series(avg_wage).to_csv(f'results/avg_{country}_wage.csv', header=['Average_Wage'])
 
 # Plot the results as a bar graph for USA vs. Immigrants
 plt.figure(figsize=(12, 8))
